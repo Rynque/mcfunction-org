41222e219621ac83296cf1664f80b2d2f6dd8f6f
Rynque <3640767735@qq.com>
2025-12-30 23:49:06 +0800

    feat: 添加首页

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="mx-auto max-w-screen-xl bg-gray-50 border-b-4 border-b-gray-100">
+    <div class="px-6 md:px-12 py-6 flex flex-col md:flex-row justify-between space-y-2 md:items-center md:space-y-0">
+      <div>
+        <NuxtLink to="/" class="text-orange-400 text-lg font-bold font-mono underline">命令月报</NuxtLink>
+        <p class="text-sm">Minecraft 技术性月刊</p>
+      </div>
+      <div class="text-[10px]/3 text-gray-600 space-y-2 md:text-right">
+        <p>本站不是 Minecraft 官方网站。未经 Mojang 或 Microsoft 批准，也不与 Mojang 或 Microsoft 关联。</p>
+        <p>提出你的想法帮我们改进网站：<a class="underline" href="mailto:support@mcfunction.org">support@mcfunction.org</a></p>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  content: {
+    experimental: { sqliteConnector: 'native' },
+  },
+  app: {
+    head: {
+      title: '命令月报',
+      htmlAttrs: {
+        lang: 'zh-CN'
+      },
+      meta: [
+        {
+          name: 'description',
+          content: 'Minecraft 技术性月刊，致力于分享最新的技术性更新介绍、入门教程和实用工具。'
+        },
+        {
+          property: 'og:title',
+          content: '命令月报 - Minecraft技术性月刊'
+        },
+        {
+          property: 'og:description',
+          content: 'Minecraft 技术性月刊，分享最新技术更新、教程和实用工具。'
+        },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:locale', content: 'zh_CN' },
+      ]
+    }
+  }
+})
